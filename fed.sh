8fb3845c810cd61a31eddef8cb0794ef7a55e21a
Jesse Gumm <sigmastar@gmail.com>
2013-01-20 11:30:19 -0600

    more defaults

--- fed.sh
+++ fed.sh
@@ -4,7 +4,7 @@
 function find_fed() {
 	if [ -e ".fed" ] ; then
 		source .fed
-		$editor $(find . -name "$1*" | grep -v "~$" | grep -v ".beam$")
+		$editor $(find . -name "$1*" | grep -v "~$" | grep -v ".beam$" | grep -v ".jpg" | grep -v ".png" | grep -v ".jpeg" | grep -v ".gif")
 	else
 		if [ `pwd` == "/" ]; then
 			echo Not in a fed project
